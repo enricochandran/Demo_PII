37f9cc677a023946c2088b30014298d047418dc3
enricochandran <52661742+enricochandran@users.noreply.github.com>
2025-07-04 14:09:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from utils import mask_name, mask_email, mask_phone, mask_address, mask_purchase_id
+from utils import mask_name, mask_email, mask_phone, mask_address, mask_purchase_id, mask_birthdate, mask_socialmedia
 import io
 
 st.title("PII Masker")
@@ -21,13 +21,58 @@ if uploaded_file:
 
         columns_to_mask_and_suffix = {
             'Name': mask_name,
+            'Full Name': mask_name,
+            'Customer Name': mask_name,
+            'User Name': mask_name,
+            'Customer Name' : mask_name,
+            'Nama': mask_name,
+            'Surname': mask_name,
+            'Nama Lengkap': mask_name,
             'Email': mask_email,
+            'Email Address': mask_email,
+            'Customer Email': mask_email,
+            'Alamat Email': mask_email,
             'Phone': mask_phone,
-            'Address': mask_address
+            'Phone Number': mask_phone,
+            'Contact Number': mask_phone,
+            'Mobile': mask_phone,
+            'Mobile Number': mask_phone,
+            'Telepon': mask_phone,
+            'Nomor Telepon': mask_phone,
+            'HP': mask_phone,
+            'No. HP': mask_phone,
+            'Address': mask_address,
+            'Home Address': mask_address,
+            'Shipping Address': mask_address,
+            'Billing Address': mask_address,
+            'Alamat': mask_address,
+            'Alamat Lengkap': mask_address,
+            'Alamat Pengiriman': mask_address,
+            'Alamat Penagihan': mask_address,
+            'Birthdate': mask_birthdate,
+            'Tanggal Lahir': mask_birthdate,
+            'Tahun Lahir': mask_birthdate,
+            'Born Date': mask_birthdate,
+            'Date of Birth': mask_birthdate,
+            'Social Media': mask_socialmedia,
+            'Instagram': mask_socialmedia,
+            'Instagram Username': mask_socialmedia,
+            'Instagram ID': mask_socialmedia,
+            'X': mask_socialmedia,
+            'X Username': mask_socialmedia,
+            'X ID': mask_socialmedia,
+            'Tiktok': mask_socialmedia,
+            'Tiktok Username': mask_socialmedia,
+            'Tiktok ID': mask_socialmedia
         }
 
         columns_to_mask_only = {
-            'PurchaseID': mask_purchase_id
+            'PurchaseID': mask_purchase_id,
+            'Transaction ID': mask_purchase_id,
+            'Order ID': mask_purchase_id,
+            'ID Pembelian': mask_purchase_id,
+            'ID Transaksi': mask_purchase_id,
+            'ID Pesanan': mask_purchase_id
         }
 
         new_columns = []
@@ -36,10 +81,10 @@ if uploaded_file:
                 mask_function = columns_to_mask_and_suffix[col]
                 df_masked[col] = df_masked[col].astype(str).apply(mask_function)
                 new_columns.append(f"{col}_PII")
-            # elif col in columns_to_mask_only:
-            #     mask_function = columns_to_mask_only[col]
-            #     df_masked[col] = df_masked[col].astype(str).apply(mask_function)
-            #     new_columns.append(col)
+            elif col in columns_to_mask_only:
+                mask_function = columns_to_mask_only[col]
+                df_masked[col] = df_masked[col].astype(str).apply(mask_function)
+                new_columns.append(col)
             else:
                 new_columns.append(col)
 
