4778d0b01ac07d5c4f9fa024ce1a189de3b2ce1b
enricochandran <52661742+enricochandran@users.noreply.github.com>
2025-07-04 14:09:30 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,6 +38,20 @@ def mask_purchase_id(text):
     text = str(text)
     return re.sub(r'\b(\d{2})\d{4}\b', r'\1****', text)
 
+def mask_socialmedia(text):
+    text = str(text)
+    masked_text = re.sub(r'(@[a-zA-Z0-9_]{1})[a-zA-Z0-9_]*', r'\1***', text)
+    masked_text = re.sub(r'(facebook\.com|twitter\.com|instagram\.com|linkedin\.com|tiktok\.com)/[a-zA-Z0-9_.-]+', r'\1/masked_profile', masked_text)
+    return masked_text
+
+def mask_birthdate(text):
+    text = str(text)
+    masked_text = re.sub(r'(\d{4})(-)(\d{2}-\d{2})', r'****\2\3', text)
+    masked_text = re.sub(r'(\d{2}/\d{2}/)(\d{4})', r'\1****', masked_text)
+    masked_text = re.sub(r'(\d{2}\.\d{2}\.)(\d{4})', r'\1****', masked_text)
+    masked_text = re.sub(r'\b(19|20)\d{2}\b', r'****', masked_text)
+    return masked_text
+
 def mask_text(text):
     masked_text = str(text)
     masked_text = mask_name(masked_text)
@@ -45,4 +59,7 @@ def mask_text(text):
     masked_text = mask_phone(masked_text)
     masked_text = mask_address(masked_text)
     masked_text = mask_purchase_id(masked_text)
+    masked_text = mask_socialmedia(masked_text)
+    masked_text = mask_birthdate(masked_text)
+    masked_text = mask_socialmedia(masked_text)
     return masked_text
